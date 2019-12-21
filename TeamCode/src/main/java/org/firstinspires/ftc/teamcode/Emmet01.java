6d167672e8e48d71599085e0bfd6fe626b6f0fa2
Emmet <knowlton@gmail.com>
2019-12-21 17:08:09 -0500

    refactored variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Emmet01.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Emmet01.java
@@ -12,75 +12,82 @@ import org.firstinspires.ftc.robotcore.external.JavaUtil;
 @TeleOp(name = "Emmet01", group = "")
 public class Emmet01 extends LinearOpMode {
 
-    private DigitalChannel digital0;
-    private DigitalChannel digital1;
-    private DigitalChannel digital2;
-    private DigitalChannel digital3;
-    private DcMotor motor0;
-    private DcMotor motor1;
-    private DcMotor motor2;
-    private DcMotor motor3;
-    private DcMotor motor0B;
-    private DcMotor motor1B;
-    private Servo servo0;
-    private Servo servo1;
-    private Servo servo2;
+    private DigitalChannel digitalMastHigh;
+    private DigitalChannel digitalJibHigh;
+    private DigitalChannel digitalMastLow;
+    private DigitalChannel digitalJibLow;
+    private DcMotor motorLeftFront;
+    private DcMotor motorRightFront;
+    private DcMotor motorLeftRear;
+    private DcMotor motorRightRear;
+    private DcMotor motorMast;
+    private DcMotor motorJib;
+    private Servo servoGrabber;
+    private Servo servoLeftWhisker;
+    private Servo servoRightWhisker;
 
 
     private ElapsedTime Timer_Loop;
     private double Time_Loop;
 
-    private double Limit_DriveSpeed;
+    private double driveSpeedLimit;
 
-    private double Control_Y_Drive;
-    private double Control_X_Strafe;
-    private double Control_R_Rotate;
+    private double controlDrive;
+    private double controlStrafe;
+    private double controlRotate;
 
-    private int State_Homing;
+    private int homingState;
 
-    private int Pos_Mast_Hold;
-    private int Pos_Mast_Max_Global;
-    private int Pos_Mast_Min_Global;
-    private int Pos_Mast_Low_JibSafe;
-    private int Pos_Mast_WiresSafe;
-    private int Pos_Mast_Max;
-    private int Pos_Mast_Min;
-    private int Pos_Mast_Current;
-    private double Power_Mast;
+    private final int mastPositionMax = 6200;
+    private final int mastPositionMin = 0;
+    private final int mastPositionJibSafe = 200;
+    private final int mastPositionBridgeSafe = 300;
 
-    private int Pos_Jib_Hold;
-    private int Pos_Jib_Max_Global;
-    private int Pos_Jib_Min_Global;
-    private int Pos_Jib_Low_MastSafe;
-    private int Pos_Jib_WiresSafe;
-    private int Pos_Jib_Max;
-    private int Pos_Jib_Min;
-    private int Pos_Jib_Current;
-    private double Power_Jib;
+    private final int jibPositionMax = 3900;
+    private final int jibPositionMin = 0;
+    private final int jibPositionPark = 350;
+    private final int jibPositionGrab = 1000;
+    private final int jibPositionPlace = 1675;
 
-    private double Position_Servo0;
+    private int mastPositionHold;
+    //private int Pos_Mast_WiresSafe;
+    //private int Pos_Mast_Max;
+    //private int Pos_Mast_Min;
+    private int mastPositionCurrent;
+    private double controlMastPower;
 
-    private int Flag_Crane_IsHomed;
-    private boolean Flag_Emmet = false;
+    private int jibPositionHold;
+    //private int Pos_Jib_Low_MastSafe;
+    //private int Pos_Jib_WiresSafe;
+    //private int Pos_Jib_Max;
+    private int jibPositionTempMin;
+    private int jibPositionCurrent;
+    private double controlJibPower;
+
+    private double grabberPosition;
+
+    private boolean flagCraneIsHomed = false;
+    private boolean flagEmmetIsCoolerThanYou = true;
 
     /**
      * This function is executed when this Op Mode is selected from the Driver Station.
      */
     @Override
     public void runOpMode() {
-        digital0 = hardwareMap.digitalChannel.get("digital0");
-        digital1 = hardwareMap.digitalChannel.get("digital1");
-        motor0 = hardwareMap.dcMotor.get("motor0");
-        motor1 = hardwareMap.dcMotor.get("motor1");
-        motor2 = hardwareMap.dcMotor.get("motor2");
-        motor3 = hardwareMap.dcMotor.get("motor3");
-        motor0B = hardwareMap.dcMotor.get("motor0B");
-        motor1B = hardwareMap.dcMotor.get("motor1B");
-        servo0 = hardwareMap.servo.get("servo0");
-        servo1 = hardwareMap.servo.get("servo1");
-        servo2 = hardwareMap.servo.get("servo2");
-        digital2 = hardwareMap.digitalChannel.get("digital2");
-        digital3 = hardwareMap.digitalChannel.get("digital3");
+        digitalMastHigh = hardwareMap.digitalChannel.get("digital0");
+        digitalJibHigh = hardwareMap.digitalChannel.get("digital1");
+        digitalMastLow = hardwareMap.digitalChannel.get("digital2");
+        digitalJibLow = hardwareMap.digitalChannel.get("digital3");
+        motorLeftFront = hardwareMap.dcMotor.get("motor0");
+        motorRightFront = hardwareMap.dcMotor.get("motor1");
+        motorLeftRear = hardwareMap.dcMotor.get("motor2");
+        motorRightRear = hardwareMap.dcMotor.get("motor3");
+        motorMast = hardwareMap.dcMotor.get("motor0B");
+        motorJib = hardwareMap.dcMotor.get("motor1B");
+        servoGrabber = hardwareMap.servo.get("servo0");
+        servoLeftWhisker = hardwareMap.servo.get("servo1");
+        servoRightWhisker = hardwareMap.servo.get("servo2");
+
 
         Initialize();
         if (opModeIsActive()) {
@@ -89,7 +96,7 @@ public class Emmet01 extends LinearOpMode {
                 Telemetry_LoopStart();
                 ReadControls();
                 // Disallow some functions when homing
-                if (State_Homing == 0) {
+                if (homingState == 0) {
                     SetCraneLimits();
                     ControlMast();
                     ControlJib();
@@ -97,7 +104,7 @@ public class Emmet01 extends LinearOpMode {
                 } else {
                     HomeCrane();
                 }
-                ControlDrivetrain();
+                controlDrivetrain();
                 Telemetry_LoopEnd();
                 telemetry.update();
             }
@@ -113,8 +120,8 @@ public class Emmet01 extends LinearOpMode {
         // Set motor directions and modes
         InitMotors();
         // Set digital i/o
-        digital0.setMode(DigitalChannel.Mode.INPUT);
-        digital1.setMode(DigitalChannel.Mode.INPUT);
+        digitalMastHigh.setMode(DigitalChannel.Mode.INPUT);
+        digitalJibHigh.setMode(DigitalChannel.Mode.INPUT);
         // Wait for Start...
         while (!isStarted()) {
             // Prompt user to press start buton.
@@ -128,50 +135,39 @@ public class Emmet01 extends LinearOpMode {
      */
     private void SetVariables() {
         Timer_Loop = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
-        // Crane Constants
-        Pos_Mast_Max_Global = 5825;
-        Pos_Mast_Min_Global = 0;
-        Pos_Jib_Max_Global = 3975;
-        Pos_Jib_Min_Global = 0;
-        Pos_Mast_Low_JibSafe = 1300;
-        Pos_Jib_Low_MastSafe = 1500;
-        Pos_Mast_WiresSafe = 2400;
-        Pos_Jib_WiresSafe = 1150;
-        // Flag Variables
-        Flag_Crane_IsHomed = 0;
     }
 
     /**
      * Describe this function...
      */
     private void InitMotors() {
-        motor0.setDirection(DcMotorSimple.Direction.REVERSE);
-        motor1.setDirection(DcMotorSimple.Direction.FORWARD);
-        motor2.setDirection(DcMotorSimple.Direction.REVERSE);
-        motor3.setDirection(DcMotorSimple.Direction.FORWARD);
-        motor0.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor0.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motor0.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor0B.setDirection(DcMotorSimple.Direction.FORWARD);
-        motor0B.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor0B.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motor0B.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motor1B.setDirection(DcMotorSimple.Direction.REVERSE);
-        motor1B.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor1B.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motor1B.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        servo0.setDirection(Servo.Direction.FORWARD);
-        servo1.setDirection(Servo.Direction.FORWARD);
-        servo2.setDirection(Servo.Direction.REVERSE);
+        motorLeftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorRightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorLeftRear.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorRightRear.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorRightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorLeftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorRightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorLeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorRightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorLeftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorRightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorLeftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorRightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorLeftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorRightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorMast.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorMast.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorMast.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorMast.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorJib.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorJib.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorJib.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorJib.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        servoGrabber.setDirection(Servo.Direction.FORWARD);
+        servoLeftWhisker.setDirection(Servo.Direction.FORWARD);
+        servoRightWhisker.setDirection(Servo.Direction.REVERSE);
     }
 
     /**
@@ -189,30 +185,30 @@ public class Emmet01 extends LinearOpMode {
     private void ReadControls() {
         if (gamepad1.left_bumper) {
             // Left Bumper = Slow
-            Limit_DriveSpeed = 0.1;
+            driveSpeedLimit = 0.1;
         } else if (gamepad1.right_bumper) {
             // Right Bumper = Maximum
-            Limit_DriveSpeed = 1;
+            driveSpeedLimit = 1;
         } else {
             // Otherwise Medium Speed
-            Limit_DriveSpeed = 0.5;
+            driveSpeedLimit = 0.5;
         }
-        Control_Y_Drive = -gamepad1.left_stick_y;
-        Control_X_Strafe = gamepad1.left_stick_x;
-        Control_R_Rotate = gamepad1.right_stick_x;
-        telemetry.addData("DRIVE STRAFE ROTATE", JavaUtil.formatNumber(Control_Y_Drive, 2) + "   " + JavaUtil.formatNumber(Control_X_Strafe, 2) + "   " + JavaUtil.formatNumber(Control_R_Rotate, 2));
+        controlDrive = -gamepad1.left_stick_y;
+        controlStrafe = gamepad1.left_stick_x;
+        controlRotate = gamepad1.right_stick_x;
+        telemetry.addData("DRIVE STRAFE ROTATE", JavaUtil.formatNumber(controlDrive, 2) + "   " + JavaUtil.formatNumber(controlStrafe, 2) + "   " + JavaUtil.formatNumber(controlRotate, 2));
     }
 
     /**
      * Describe this function...
      */
     private void Telemetry_LoopStart() {
-        if (Flag_Crane_IsHomed == 0) {
+        if (flagCraneIsHomed == false) {
             telemetry.addData("Crane Status", "===>  NOT HOMED  <===");
         } else {
             telemetry.addData("Crane Status", "homed");
         }
-        if (State_Homing == 0) {
+        if (homingState == 0) {
             // Minimize loop time when homing by
             // not reading unnecessary sensors
         }
@@ -227,13 +223,13 @@ public class Emmet01 extends LinearOpMode {
         // !! Disable Sensor Polling for more speed !!
         // Best to read only values needed each loop
         // and store them in variables
-        if (State_Homing == 0) {
+        if (homingState == 0) {
             // Minimize loop time when homing by
             // not reading unnecessary sensors
-            telemetry.addData("Limit Switches", digital0.getState() + "   " + digital1.getState() + "   " + digital2.getState() + "   " + digital3.getState());
-            telemetry.addData("Encoders B", Pos_Mast_Current + "   " + Pos_Jib_Current);
+            telemetry.addData("Limit Switches", digitalMastHigh.getState() + "   " + digitalJibHigh.getState() + "   " + digitalMastLow.getState() + "   " + digitalJibLow.getState());
+            telemetry.addData("Encoders B", mastPositionCurrent + "   " + jibPositionCurrent);
         }
-        telemetry.addData("Servo", JavaUtil.formatNumber(Position_Servo0, 2));
+        telemetry.addData("Servo", JavaUtil.formatNumber(grabberPosition, 2));
     }
 
     /**
@@ -269,33 +265,34 @@ public class Emmet01 extends LinearOpMode {
     /**
      * Describe this function...
      */
-    private void ControlDrivetrain() {
-        double leftFrontPower, rightFrontPower, leftRearPower, rightRearPower, maxPower;
+    private void controlDrivetrain() {
+        double leftFrontPower, rightFrontPower, leftRearPower, rightRearPower, driveMaxPower;
 
-        leftFrontPower = Control_Y_Drive + Control_X_Strafe + Control_R_Rotate;
-        rightFrontPower = Control_Y_Drive - Control_X_Strafe - Control_R_Rotate;
-        leftRearPower = Control_Y_Drive - Control_X_Strafe + Control_R_Rotate;
-        rightRearPower = Control_Y_Drive + Control_X_Strafe - Control_R_Rotate;
+        //setting power levels based on drive controls
+        leftFrontPower = controlDrive + controlStrafe + controlRotate;
+        rightFrontPower = controlDrive - controlStrafe - controlRotate;
+        leftRearPower = controlDrive - controlStrafe + controlRotate;
+        rightRearPower = controlDrive + controlStrafe - controlRotate;
 
         //find max and normalize
-        maxPower = Math.max(Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower)), Math.max(Math.abs(leftRearPower), Math.abs(rightRearPower)));
-        maxPower = Math.max(maxPower, 1);
-        leftFrontPower /= maxPower;
-        rightFrontPower /= maxPower;
-        leftRearPower /= maxPower;
-        rightRearPower /= maxPower;
+        driveMaxPower = Math.max(Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower)), Math.max(Math.abs(leftRearPower), Math.abs(rightRearPower)));
+        driveMaxPower = Math.max(driveMaxPower, 1);
+        leftFrontPower /= driveMaxPower;
+        rightFrontPower /= driveMaxPower;
+        leftRearPower /= driveMaxPower;
+        rightRearPower /= driveMaxPower;
 
         //limit motors to max speed
-        leftFrontPower *= Limit_DriveSpeed;
-        rightFrontPower *= Limit_DriveSpeed;
-        leftRearPower *= Limit_DriveSpeed;
-        rightRearPower *= Limit_DriveSpeed;
+        leftFrontPower *= driveSpeedLimit;
+        rightFrontPower *= driveSpeedLimit;
+        leftRearPower *= driveSpeedLimit;
+        rightRearPower *= driveSpeedLimit;
 
         //set motor powers
-        motor0.setPower(leftFrontPower);
-        motor1.setPower(rightFrontPower);
-        motor2.setPower(leftRearPower);
-        motor3.setPower(rightRearPower);
+        motorLeftFront.setPower(leftFrontPower);
+        motorRightFront.setPower(rightFrontPower);
+        motorLeftRear.setPower(leftRearPower);
+        motorRightRear.setPower(rightRearPower);
 
     }
 }
