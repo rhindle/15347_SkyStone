719ff370879341db1e66c3b740fe74d4daaae5ec
Emmet <knowlton@gmail.com>
2020-01-15 21:02:54 -0500

    changed skystone distances, added new presets

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Override_Red_Double.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Auto: Red / Double", group = "")
+public class Autonomous_Override_Red_Double extends Emmet_Autonomous_New {
+
+    @Override
+    void setAutonomousVariables() {
+        //1 = blue, 2 = red
+        autoAlliance = 2;
+        // 1 is foundation, 2 is quarry
+        autoSide = 3;
+        //1 is near, 2 is far
+        autoParkingPosition = 2;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Override_Red_Skystone_Near.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Auto: Red / Skystone / Near", group = "")
+public class Autonomous_Override_Red_Skystone_Near extends Emmet_Autonomous_New {
+
+    @Override
+    void setAutonomousVariables() {
+        //1 = blue, 2 = red
+        autoAlliance = 2;
+        // 1 is foundation, 2 is quarry
+        autoSide = 2;
+        //1 is near, 2 is far
+        autoParkingPosition = 1;
+    }
+}
