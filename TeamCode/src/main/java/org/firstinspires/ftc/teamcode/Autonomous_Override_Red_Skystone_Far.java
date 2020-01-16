ba868b0c0ce5ff0850cd65703b3983704e2f2eef
rhindle <knowlton@gmail.com>
2020-01-15 20:03:13 -0500

    refactoring for saturday

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Override_Red_Skystone_Far.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Override_Red_Skystone_Far.java
@@ -2,13 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "Auto: Blue / Skystone", group = "")
-public class Autonomous_Override_Blue_Skystone extends Emmet_Autonomous {
+@Autonomous(name = "Auto: Red / Skystone / Far", group = "")
+public class Autonomous_Override_Red_Skystone_Far extends Emmet_Autonomous_New {
 
     @Override
     void setAutonomousVariables() {
         //1 = blue, 2 = red
-        autoAlliance = 1;
+        autoAlliance = 2;
         // 1 is foundation, 2 is quarry
         autoSide = 2;
         //1 is near, 2 is far
